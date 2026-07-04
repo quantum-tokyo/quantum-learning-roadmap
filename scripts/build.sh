681382e806cb26ac1aaffb0c8b1d10ed5f2f9b18
DaikiMurata <Daiki.Murata@ibm.com>
2026-07-04 19:49:06 +0900

    migrate jupyter-book to 2.x

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-jupyter-book build ./src/ --all
+# The MyST engine (Jupyter Book 2) builds from within the project directory
+# that contains myst.yml, so we run the build inside ./src.
+cd "$(dirname "$0")/../src" && jupyter book build --html
